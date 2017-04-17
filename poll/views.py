2dfc4c8b20ca3bedcebad7165e1036f8769e19c4
lionder <lionder.97@bk.ru>
2017-04-18 02:38:32 +0300

    front-end improvements (e.g. bootstrap)

--- poll/views.py
+++ poll/views.py
@@ -1,12 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.template.context_processors import csrf
 from django.views.decorators.csrf import csrf_protect
+from django.contrib.auth.decorators import login_required
 
-from .models import Poll, Question, Choice, FIELD_TYPE_CHOICES
+from .models import Poll, Choice, FIELD_TYPE_CHOICES
 from django.contrib.auth.models import User
-
-from django.shortcuts import render_to_response
 
 
 def index(request):
@@ -24,14 +22,16 @@ def register(request):
     return render(request, "registration/registration_complete.html")
 
 
+@login_required
 @csrf_protect
 def poll(request, poll_id):
     curr_poll = Poll.objects.get(id=poll_id)
     mdict = {'poll': curr_poll, 'types': FIELD_TYPE_CHOICES}
     mdict.update(csrf(request))
-    return render_to_response('poll.html', mdict)
+    return render(request, 'poll.html', mdict)
 
 
+@login_required
 @csrf_protect
 def answer(request, poll_id):
     if request.method == 'POST':
@@ -52,15 +52,15 @@ def answer(request, poll_id):
             'notification': "Something went wrong! Go back home...",
             'notification_type': 'danger'
         }
-    return render_to_response('notification.html', response)
+    return render(request, 'notification.html', response)
 
 
 def dashboard(request,):
     polls = Poll.objects.all()
-    return render_to_response('admin/dashboard.html', {'polls': polls})
+    return render(request, 'admin/dashboard.html', {'polls': polls})
 
 
 def dashboard_poll(request, poll_id):
     curr_poll = Poll.objects.get(id=poll_id)
     polls = Poll.objects.all()
-    return render_to_response('admin/dashboard.html', {'curr_poll': curr_poll, 'polls': polls})
+    return render(request, 'admin/dashboard.html', {'curr_poll': curr_poll, 'polls': polls})
